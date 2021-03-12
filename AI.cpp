9cd2f44e598d083f1874d27abd42cad286b0b5f6
spee-chee <53122886+spee-chee@users.noreply.github.com>
2021-03-12 01:10:07 +0000

    Formated with clang-format and updated mcts selection class method.

--- AI.cpp
+++ AI.cpp
@@ -1,250 +1,253 @@
 using namespace std;
-#include <array>
-#include <vector>
-#include <unordered_map>
-#include <random>
-#include <algorithm>
 #include "board.h"
+
+#include <algorithm>
+#include <array>
+#include <random>
+#include <unordered_map>
+#include <vector>
 
 #define EXPLORATIONCONST 1.414214
 struct Node
 {
 
-    vector<Node*> ChildNodes = {}; // Pointers to each child node
-    vector<Node*> ParentNodes = {}; // Pointers to all parent nodes
-    unsigned int Hash;
+  // TODO
+  // change ChildNodes to a set or map rather than a vector
+  vector<Node *> ChildNodes = {};  // Pointers to each child node
+  vector<Node *> ParentNodes = {}; // Pointers to all parent nodes
+  unsigned int Hash;
 
-    short int wins = 0;
-    short int visits = 0;
-    bool expanded = false;
-    short int depth;
-    char move;
-    PlayerPiece PlayerTurn;
-    
-    Node(Node* parent, int move)
-    {
-        ParentNodes.push_back(parent);
-        depth = parent -> depth + 1;
-        PlayerTurn = ((parent->PlayerTurn)==PlayerOne)?PlayerTwo:PlayerOne;
+  short int wins = 0;
+  short int visits = 0;
+  bool expanded = false;
+  short int depth;
+  char move;
+  PlayerPiece PlayerTurn;
 
-    }
-    /*
-    Node* deep_copy()
-    {
-        Node copy = Node(ParentNodes[0]);
+  Node(Node *parent, int move)
+  {
+    ParentNodes.push_back(parent);
+    depth = parent->depth + 1;
+    PlayerTurn = ((parent->PlayerTurn) == PlayerOne) ? PlayerTwo : PlayerOne;
+  }
+  /*
+  Node* deep_copy()
+  {
+      Node copy = Node(ParentNodes[0]);
 
-        for(int i = 1; i != ParentNodes.size(); ++i)
-            copy.ParentNodes.push_back(ParentNodes[i]);
+      for(int i = 1; i != ParentNodes.size(); ++i)
+          copy.ParentNodes.push_back(ParentNodes[i]);
 
-        for(int i = 0; i != ChildNodes.size(); ++i)
-            copy.ChildNodes.push_back(ChildNodes[i]);
-        
-        copy.wins = wins;
-        copy.visits = visits;
-        copy.Hash = Hash;
+      for(int i = 0; i != ChildNodes.size(); ++i)
+          copy.ChildNodes.push_back(ChildNodes[i]);
 
-    }
-    */
+      copy.wins = wins;
+      copy.visits = visits;
+      copy.Hash = Hash;
+
+  }
+  */
 };
 
 struct TranspositionTable // contains map of all nodes which have been explored
 {
-    unordered_map<unsigned int, Node*> table = {};
+  unordered_map<unsigned int, Node *> table = {};
 
-    //Creates new node if hasn't been explored.
-    Node* create_node(unsigned int hash, Node* parent, char move)
+  // Creates new node if hasn't been explored.
+  Node *create_node(unsigned int hash, Node *parent, char move)
+  {
+    if(table.find(hash) == table.end())
     {
-        if (table.find(hash) == table.end())
-        {
-            table[hash] = new Node(parent, move);
-        }
-        return table[hash];
+      table[hash] = new Node(parent, move);
     }
+    return table[hash];
+  }
 };
 
 class ZorbistHashing
 {
-    
-    array<array<unsigned int,2>,42> ZorbistTable = {};
 
-    public:
-    ZorbistHashing()
+  array<array<unsigned int, 2>, 42> ZorbistTable = {};
+
+public:
+  ZorbistHashing()
+  {
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> distrib(1, 4294967295);
+
+    // generates a random number for each piece in each position
+    for(int i = 0; i != 42; ++i)
     {
-        random_device rd;
-        mt19937 gen(rd());
-        uniform_int_distribution<> distrib(1,4294967295);
-        
-        //generates a random number for each piece in each position
-        for(int i = 0; i!= 42; ++i) 
-        {
-             ZorbistTable[i][0] = distrib(gen);
-             ZorbistTable[i][1] = distrib(gen);
-        }
+      ZorbistTable[i][0] = distrib(gen);
+      ZorbistTable[i][1] = distrib(gen);
     }
-    unsigned int get_zorbist_hash(PlayerPiece player, int x, int y, unsigned int hash)
-    {
-        
-        return ZorbistTable[x + y*7][player]^hash;
-    }
+  }
+  unsigned int get_zorbist_hash(PlayerPiece player, int x, int y, unsigned int hash)
+  {
+
+    return ZorbistTable[x + y * 7][player] ^ hash;
+  }
 };
-
 
 class MCTS
 {
-    Node RootNode = Node(nullptr, 0);
-    Board RootBoard = Board();
+  Node RootNode = Node(nullptr, 0);
+  Board RootBoard = Board();
 
-    Node CurrentNode = RootNode;
-    Board CurrentBoard = RootBoard;
+  Node CurrentNode = RootNode;
+  Board CurrentBoard = RootBoard;
 
-    TranspositionTable TransTable = TranspositionTable();
-    
-    ZorbistHashing HashGen = ZorbistHashing();
-    MCTS()
+  TranspositionTable TransTable = TranspositionTable();
+
+  ZorbistHashing HashGen = ZorbistHashing();
+  MCTS()
+  {
+    RootNode.Hash = 0;
+  }
+
+  vector<int> get_moves()
+  {
+    vector<int> ValidMoves = {};
+    for(int i = 0; i != 7; ++i)
     {
-        RootNode.Hash = 0;
+      if(CurrentBoard.BoardTop[i] != -1)
+      {
+        ValidMoves.push_back(i);
+      }
+    }
+    return ValidMoves;
+  }
+
+  double calc_UCT(Node node)
+  {
+    /*  UCT normally only works for trees where nodes have a single parent,
+        so I'm not sure if totally all of the parent visits up will work    */
+
+    int ParentVisits = 0;
+    for(int i = 0; i != node.ParentNodes.size(); ++i)
+    {
+      ParentVisits += node.ParentNodes[i]->visits;
     }
 
-    
+    double UCT = node.wins / node.visits + EXPLORATIONCONST * sqrt(log(ParentVisits) / node.visits);
 
-    vector<int> get_moves()
+    return UCT;
+  }
+
+  void selection()
+  {
+    double MaxUCT;
+    double uct;
+    Node *NextNode;
+
+    while(CurrentNode.expanded = true)
     {
-        vector<int> ValidMoves = {};
-        for(int i = 0; i!=7; ++i)
-        {
-            if(CurrentBoard.BoardTop[i] != -1)
-            {
-                ValidMoves.push_back(i);
-            }
-        }
-        return ValidMoves;
+      MaxUCT = 0;
 
+      for(Node *pNode: CurrentNode.ChildNodes)
+      {
+        uct = calc_UCT(*pNode);
+        if(uct > MaxUCT)
+        {
+          NextNode = pNode;
+          MaxUCT = uct;
+        }
+      }
+
+      CurrentBoard.drop_piece(NextNode->move, NextNode->PlayerTurn);
+      CurrentNode = *NextNode;
     }
 
-    double calc_UCT(Node node)
-    {
-        /*  UCT normally only works for trees where nodes have a single parent, 
-            so I'm not sure if totally all of the parent visits up will work    */
-
-        int ParentVisits = 0; 
-        for(int i = 0; i != node.ParentNodes.size(); ++i) 
-        {
-            ParentVisits += node.ParentNodes[i] -> visits;
-        }
-
-        double UCT = node.wins / node.visits + EXPLORATIONCONST * sqrt(log(ParentVisits)/ node.visits);
-
-        return UCT;
-    }
-
-    void selection()
-    {
-        double MaxUCT;
-        double uct;
-        Node* NextNode;
-        
-        while(CurrentNode.expanded = true)
-        {
-            MaxUCT = 0;
-            
-            for(Node* node: CurrentNode.ChildNodes)
-            {
-                uct = calc_UCT(*node);
-                if (uct>MaxUCT)
-                {
-                    NextNode = node;
-                    MaxUCT = uct;
-                }
-            }
-            
-
-            CurrentBoard.drop_piece(NextNode-> move, NextNode -> PlayerTurn);
-            CurrentNode = *NextNode;
-        }
-
-        /*
-       double max_uct = 0; 
-       double uct;
-       Node* node;
-       for(pair<unsigned int, Node*> NodePair: TransTable.table)
+    /*
+   double max_uct = 0;
+   double uct;
+   Node* node;
+   for(pair<unsigned int, Node*> NodePair: TransTable.table)
+   {
+       uct = calc_UCT((*NodePair.second));
+       if ( uct > max_uct)
        {
-           uct = calc_UCT((*NodePair.second));
-           if ( uct > max_uct) 
-           {
-               node = NodePair.second;
-               max_uct = uct;
-           }
+           node = NodePair.second;
+           max_uct = uct;
        }
+   }
 
-       return node;
-        */
+   return node;
+    */
+  }
+
+  void explansion()
+  {
+
+    vector<int> ValidMoves = get_moves();
+    unsigned int NewHash;
+    Node *pNewChild;
+
+    // Loops through each valid move and adds it to ChildNodes attribute
+    for(int move: ValidMoves)
+    {
+      NewHash = HashGen.get_zorbist_hash((CurrentNode.PlayerTurn == PlayerOne) ? PlayerTwo : PlayerOne, move, CurrentBoard.BoardTop[move],
+                                         CurrentNode.Hash);
+      pNewChild = TransTable.create_node(NewHash, &CurrentNode, (char) move);
+
+      // Check if the node already exists as a child node before adding.
+      if(find(CurrentNode.ChildNodes.begin(), CurrentNode.ChildNodes.end(), pNewChild) == CurrentNode.ChildNodes.end())
+      {
+        CurrentNode.ChildNodes.push_back(pNewChild);
+      }
     }
 
-    void explansion()
+    CurrentNode.expanded = true;
+  }
+
+  PlayerPiece simulation()
+  {
+
+    bool complete;
+    int move;
+    unsigned int NewHash;
+    Node *pNewChild;
+
+    // TODO
+    // Make it so it changes the board state after changing current node
+    // Implement backpropagation after each child finishes exploring
+    for(Node *pChildNode: CurrentNode.ChildNodes) // note sure if this will work.
     {
-        
+      CurrentNode = *pChildNode;
+      complete = false;
+      while(!complete)
+      {
         vector<int> ValidMoves = get_moves();
-        unsigned int NewHash;
-        Node* pNewChild;
-        for(int move: ValidMoves)
-        {
-            NewHash = HashGen.get_zorbist_hash((CurrentNode.PlayerTurn==PlayerOne)?PlayerTwo:PlayerOne, move, CurrentBoard.BoardTop[move], CurrentNode.Hash);
-            pNewChild = TransTable.create_node(NewHash, &CurrentNode, (char) move );
 
-            // check if the node already exists as a child node before adding.
-            if (find(CurrentNode.ChildNodes.begin(), CurrentNode.ChildNodes.end(),pNewChild)==CurrentNode.ChildNodes.end())
-            {
-                CurrentNode.ChildNodes.push_back(pNewChild);
-            }
+        random_device rd;
+        mt19937 gen(rd());
+        uniform_int_distribution<> distrib(0, ValidMoves.size() - 1);
+        move = distrib(gen);
+
+        NewHash = HashGen.get_zorbist_hash((CurrentNode.PlayerTurn == PlayerOne) ? PlayerTwo : PlayerOne, move, CurrentBoard.BoardTop[move],
+                                           CurrentNode.Hash);
+        pNewChild = TransTable.create_node(NewHash, &CurrentNode, (char) move);
+
+        if(find(CurrentNode.ChildNodes.begin(), CurrentNode.ChildNodes.end(), pNewChild) == CurrentNode.ChildNodes.end())
+        {
+          CurrentNode.ChildNodes.push_back(pNewChild);
         }
 
-        CurrentNode.expanded = true;
+        CurrentNode = *pNewChild;
+      }
     }
 
+    // will randomly play until game is complete
+  }
 
-    PlayerPiece simulation()
-    {
+  bool backpropagation()
+  {
+  }
 
-        bool complete = false;
-        for(Node* ChildNode: CurrentNode.ChildNodes)
-        {
-            CurrentNode = *ChildNode;
-            while(!complete) 
-            {
-                vector<int> ValidMoves = get_moves();
-
-                random_device rd;
-                mt19937 gen(rd());
-                uniform_int_distribution<> distrib(0,ValidMoves.size()-1);
-                int move =  distrib(gen);
-                
-                unsigned int NewHash = HashGen.get_zorbist_hash((CurrentNode.PlayerTurn==PlayerOne)?PlayerTwo:PlayerOne, 
-                                                                move, CurrentBoard.BoardTop[move], CurrentNode.Hash);
-                TransTable.create_node(NewHash, &CurrentNode, (char) move);
-
-
-
-            }
-
-
-        }
-
-        // will randomly play until game is complete
-        
-        
-
-
-    }
-
-    bool backpropagation()
-    {
-
-    }
-
-    void play_move()
-    {
-        //change root node
-        //playesr turn
-    }
-
+  void play_move()
+  {
+    // change root node
+    // playesr turn
+  }
 };
--- board.cpp
+++ board.cpp
@@ -1,165 +1,159 @@
-#include <vector>
 #include <array>
 #include <iostream>
+#include <vector>
 
+// TODO
+// Change it so that p1 = 0, p2 = 1, and empty = 2
 enum PlayerPiece
 {
-    Empty = 0,
-    PlayerOne = 1,
-    PlayerTwo = 2,
+  Empty = 0,
+  PlayerOne = 1,
+  PlayerTwo = 2,
 };
 
 using namespace std;
 struct Board
 {
-    array<PlayerPiece, 42> board = {Empty}; // board as a 1d array
-    array<short int, 7> BoardTop = {5,5,5,5,5,5,5};
-    
-    //Insert Piece. Return True if success. Else return false
-    bool drop_piece(int x, PlayerPiece player)     
-    {
-        if(BoardTop[x]!= -1){
-            board[flatten(x, BoardTop[x])] = player;
-            BoardTop[x]--;
-            return true;
-        }
-        else return false;
-    } 
+  array<PlayerPiece, 42> board = {Empty}; // board as a 1d array
+  array<short int, 7> BoardTop = {5, 5, 5, 5, 5, 5, 5};
 
-    int flatten(int x, int y)
+  // Insert Piece. Return True if success. Else return false
+  bool drop_piece(int x, PlayerPiece player)
+  {
+    if(BoardTop[x] != -1)
     {
-        return x + y*7 ; 
+      board[flatten(x, BoardTop[x])] = player;
+      BoardTop[x]--;
+      return true;
+    }
+    else
+      return false;
+  }
+
+  int flatten(int x, int y)
+  {
+    return x + y * 7;
+  }
+
+  bool check_win(int xPos, PlayerPiece player)
+  {
+    int yPos = BoardTop[xPos] + 1;
+
+    // horizontal
+    for(int i = xPos >= 4 ? xPos - 3 : 0; i < 4 && i <= xPos; ++i)
+    {
+      // cout << "horiz" << endl;
+      // cout << board[flatten(i,yPos)] << board[flatten(i+1,yPos)] << board[flatten(i+2,yPos)] << board[flatten(i+3,yPos)]  << endl;
+
+      if(board[flatten(i, yPos)] == board[flatten(i + 1, yPos)] && board[flatten(i + 1, yPos)] == board[flatten(i + 2, yPos)] &&
+         board[flatten(i + 2, yPos)] == board[flatten(i + 3, yPos)])
+      {
+
+        return true;
+      }
     }
 
-    bool check_win(int xPos, PlayerPiece player)
+    // verticle
+    if(yPos <= 2) // if smaller than 2, than it cant be a complete verticle line of 4
     {
-        int yPos = BoardTop[xPos]+1;
+      // cout << "vert" << endl;
+      // cout << board[flatten(xPos,yPos)] << board[flatten(xPos,yPos+1)] << board[flatten(xPos,yPos+2)] << board[flatten(xPos,yPos+3)];
 
-        // horizontal
-        for(int i = xPos>=4?xPos-3:0; i< 4 && i<= xPos; ++i )
-        {
-            //cout << "horiz" << endl;
-            //cout << board[flatten(i,yPos)] << board[flatten(i+1,yPos)] << board[flatten(i+2,yPos)] << board[flatten(i+3,yPos)]  << endl;
-
-            if(board[flatten(i,yPos)] == board[flatten(i+1,yPos)] 
-            && board[flatten(i+1,yPos)] == board[flatten(i+2,yPos)] 
-            && board[flatten(i+2,yPos)] == board[flatten(i+3,yPos)] )
-            {
-                
-                return true;
-            } 
-        }
-
-        // verticle
-        if(yPos <=2) //if smaller than 2, than it cant be a complete verticle line of 4
-        {
-            //cout << "vert" << endl;
-            //cout << board[flatten(xPos,yPos)] << board[flatten(xPos,yPos+1)] << board[flatten(xPos,yPos+2)] << board[flatten(xPos,yPos+3)];
-
-            if(board[flatten(xPos,yPos)] == board[flatten(xPos,yPos+1)] 
-            && board[flatten(xPos,yPos+1)] == board[flatten(xPos,yPos+2)] 
-            && board[flatten(xPos,yPos+2)] ==board[flatten(xPos,yPos+3)])
-            {
-                return true;
-            }
-        }
-
-        int pos = flatten(xPos, yPos);
-
-        // diag downwards
-        int i;
-        if (xPos >=3 and yPos >=3) i = pos - 24;
-        else i = pos - 8*((xPos <= yPos)? xPos: yPos);
-        
-        do 
-        {
-            if(board[i] == board[i + 8] 
-            && board[i+8] == board[i+16]
-            && board[i+16] == board[i+24])
-            {
-                return true;
-            }
-            i+=8;
-        }
-        while (i%7!=0 and i<=17 and i<=pos );
-
-
-        // diag upwards
-
-        if (xPos >=3 and yPos <=2) i = pos +6*3;
-        else i = pos + 6*((xPos <= 5-yPos)? xPos: yPos);
-        
-        do 
-        {
-            if(board[i] == board[i - 6] 
-            && board[i-6 ] == board[i-12]
-            && board[i-12] == board[i-18])
-            {
-                return true;
-            }
-            i-=6;
-        }
-        while (i%7!=0 and i>=21 and (i%7) <=3 and i>=pos );
-
-
-
-        return false;
+      if(board[flatten(xPos, yPos)] == board[flatten(xPos, yPos + 1)] && board[flatten(xPos, yPos + 1)] == board[flatten(xPos, yPos + 2)] &&
+         board[flatten(xPos, yPos + 2)] == board[flatten(xPos, yPos + 3)])
+      {
+        return true;
+      }
     }
 
-    void display_board()
+    int pos = flatten(xPos, yPos);
+
+    // diag downwards
+    int i;
+    if(xPos >= 3 and yPos >= 3)
+      i = pos - 24;
+    else
+      i = pos - 8 * ((xPos <= yPos) ? xPos : yPos);
+
+    do
     {
-        for (int y = 0; y != 6; ++y)
-        {
-            for (int x = 0; x != 7; x++)
-            {
-                cout << board[flatten(x,y)] << " ";
-            }
-            cout << endl;
-        }
-    }
+      if(board[i] == board[i + 8] && board[i + 8] == board[i + 16] && board[i + 16] == board[i + 24])
+      {
+        return true;
+      }
+      i += 8;
+    } while(i % 7 != 0 and i <= 17 and i <= pos);
 
-    Board* deep_copy()
+    // diag upwards
+
+    if(xPos >= 3 and yPos <= 2)
+      i = pos + 6 * 3;
+    else
+      i = pos + 6 * ((xPos <= 5 - yPos) ? xPos : yPos);
+
+    do
     {
-        Board copy = Board();
-        copy.board = board;
-        copy.BoardTop = BoardTop;
+      if(board[i] == board[i - 6] && board[i - 6] == board[i - 12] && board[i - 12] == board[i - 18])
+      {
+        return true;
+      }
+      i -= 6;
+    } while(i % 7 != 0 and i >= 21 and (i % 7) <= 3 and i >= pos);
 
-        return &copy;
+    return false;
+  }
+
+  void display_board()
+  {
+    for(int y = 0; y != 6; ++y)
+    {
+      for(int x = 0; x != 7; x++)
+      {
+        cout << board[flatten(x, y)] << " ";
+      }
+      cout << endl;
     }
+  }
 
+  Board *deep_copy()
+  {
+    Board copy = Board();
+    copy.board = board;
+    copy.BoardTop = BoardTop;
 
+    return &copy;
+  }
 };
 
 int main()
 {
-    Board game = Board();
-    bool win = false;
-    int pos;
-    while (true)
+  Board game = Board();
+  bool win = false;
+  int pos;
+  while(true)
+  {
+    game.display_board();
+    cout << "player 1" << endl;
+    cin >> pos;
+    game.drop_piece(pos, PlayerOne);
+
+    if(game.check_win(pos, PlayerOne))
     {
-        game.display_board();
-        cout << "player 1" << endl;
-        cin >> pos; 
-        game.drop_piece(pos, PlayerOne);
-
-        if (game.check_win(pos, PlayerOne))
-        {
-            game.display_board();
-            cout << "p1 wins";
-            break;
-        }
-        game.display_board();
-        cout << "player 2" << endl;
-        cin >> pos; 
-        game.drop_piece(pos, PlayerTwo);
-
-        if (game.check_win(pos, PlayerTwo))
-        {
-            game.display_board();
-            cout << "p2 wins";
-            break;
-        }
-
+      game.display_board();
+      cout << "p1 wins";
+      break;
     }
-    return 0;
+    game.display_board();
+    cout << "player 2" << endl;
+    cin >> pos;
+    game.drop_piece(pos, PlayerTwo);
+
+    if(game.check_win(pos, PlayerTwo))
+    {
+      game.display_board();
+      cout << "p2 wins";
+      break;
+    }
+  }
+  return 0;
 }
