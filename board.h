17af0e0710ab573183d475d10ddbce62bdf1c9e7
spee-chee <53122886+spee-chee@users.noreply.github.com>
2021-03-12 02:07:33 +0000

    Updated to reflect changes in board.cpp

--- board.h
+++ board.h
@@ -1,29 +1,28 @@
 #pragma once
 
-#include <vector>
 #include <array>
+#include <vector>
 enum PlayerPiece
 {
-    Empty = 0,
-    PlayerOne = 1,
-    PlayerTwo = 2,
+  Empty = 0,
+  PlayerOne = 1,
+  PlayerTwo = 2,
 };
 
 using namespace std;
 struct Board
 {
-    array<PlayerPiece, 42> Board;
-    array<short int, 7> BoardTop;
+  array<PlayerPiece, 42> board;
+  array<short int, 7> BoardTop;
 
-    //Insert Piece. Return True if success. Else return false
-    bool drop_piece(int x, PlayerPiece player);
+  // Insert Piece. Return True if success. Else return false
+  bool drop_piece(int x, PlayerPiece player);
 
-    int flatten(int x, int y);
+  int flatten(int x, int y);
 
-    bool check_win(){}
+  bool check_win(int xPos, PlayerPiece player);
 
-    void display_board(){}
+  void display_board();
 
-
+  Board *deep_copy();
 };
-
